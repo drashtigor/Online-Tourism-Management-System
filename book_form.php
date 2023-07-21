307a64762105b9583d41c6e46e69afd748a6e1ef
drashtigor <drashtigor11@gmail.com>
2023-07-21 17:04:43 +0530

    travel

---
+++ book_form.php
@@ -0,0 +1,24 @@
+<?php
+
+   $connection = mysqli_connect('localhost:3307','root','','users');
+
+   if(isset($_POST['send'])){
+      $name = $_POST['name'];
+      $email = $_POST['email'];
+      $phone = $_POST['phone'];
+      $address = $_POST['address'];
+      $location = $_POST['location'];
+      $guests = $_POST['guests'];
+      $arrivals = $_POST['arrivals'];
+      $leaving = $_POST['leaving'];
+
+      $request = " insert into users(name, email, phone, address, location, guests, arrivals, leaving) values('$name','$email','$phone','$address','$location','$guests','$arrivals','$leaving') ";
+      mysqli_query($connection, $request);
+
+      header('location:pay.php'); 
+
+   }else{
+      echo 'something went wrong please try again!';
+   }
+
+?>
